fcf67beacf61fb98020b87b830f9d7cde1ea9bc3
Ahmed4ls <ahmed.al-shamary0044@stud.hkr.se>
2025-03-22 00:29:17 +0100

    Sprint1 code (right concept but without GUI)
    I did everyting except the while loop. Sebastian did that.

--- sprint1.py
+++ sprint1.py
@@ -1,17 +1,22 @@
 def main():
-    sex = input("Enter your sex (man/woman): ")
+
+    user_data = {}
+
+
+    user_data['sex'] = input("Enter your sex (man/woman): ").strip().lower()
     print()
-    age = int(input("Enter your age: "))
+    user_data['age'] = int(input("Enter your age: ").strip())
     print()
-    weight = float(input("Enter your weight (kg): "))
+    user_data['weight'] = float(input("Enter your weight (kg): ").strip())
     print()
-    target_weight = float(input("Enter your target weight (kg): "))
+    user_data['target_weight'] = float(input("Enter your target weight (kg): ").strip())
     print()
-    weeks = int(input("How fast do you want to achieve your goal (weeks)?: "))
+    user_data['weeks'] = int(input("How fast do you want to achieve your goal (weeks)?: ").strip())
     print()
-    height = float(input("Enter your height (cm): "))
+    user_data['height'] = float(input("Enter your height (cm): ").strip())
     print()
-    
+
+
     print("How active are you during work?")
     print("1: Not active (sitting all day)")
     print("2: Slightly active (office work or similar)")
@@ -19,8 +24,9 @@ def main():
     print("4: Active (gym instructor or similar)")
     print("5: Very active (craftsman or similar)")
     print()
-    work_activity = int(input("Select your work activity level (1-5): "))
+    user_data['work_activity'] = int(input("Select your work activity level (1-5): ").strip())
     print()
+
 
     print("How active are you during your spare time?")
     print("1: Not active (no exercise)")
@@ -29,7 +35,7 @@ def main():
     print("4: Active (moderate exercise 3-4 times a week)")
     print("5: Very active (hard exercise at least 4 times a week)")
     print()
-    spare_time_activity = int(input("Select your spare time activity level (1-5): "))
+    user_data['spare_time_activity'] = int(input("Select your spare time activity level (1-5): ").strip())
     print()
     
     while True:
