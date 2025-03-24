034f3b364ee6fd07cc0addfdcba41a905206498b
Ahmed4ls <ahmed.al-shamary0044@stud.hkr.se>
2025-03-24 16:33:07 +0100

    Testar ogiltiga uppdateringar
    Detta Unit Test gjordes för Sprint 1 koden vi valde att inte använda oss efter(terminal koden).

---
+++ Tests/InvalidUpdateTest.py
@@ -0,0 +1,15 @@
+import unittest
+from unittest.mock import patch
+from Main import main
+
+class TestInvalidUpdate(unittest.TestCase):
+    @patch('builtins.input', side_effect=[
+        'man', '22', '80', '75', '12', '175',
+        '4', '5', 'yes', 'wrong_input', 'no'
+    ])
+    def test_invalid_update(self, mock_input):
+        user_data = main()
+        self.assertEqual(user_data['age'], 22)
+
+if name == "main":
+    unittest.main()
